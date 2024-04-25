f96054a9510af65902b352eaad16b01cabdd0e20
julian-urbano <urbano.julian@gmail.com>
2024-04-25 17:14:57 +0200

    Simplify RBO computation on TREC data so that breaking at random and by docID are together on the same file. There was no need to separate, as all tie-aware variants are the same!

--- src/11-rbo-trec.R
+++ src/11-rbo-trec.R
@@ -1,66 +1,71 @@
 source("src/common.R")
 source("rbo/rbo.R")
 
-for(breaker in .TIE_BREAKER) {
-  for(year in .WEB_YEAR) {
-    print(c(breaker, year))
+for(year in .WEB_YEAR) {
+  print(year)
 
-    set.seed(0)
+  set.seed(0)
 
-    # Read runs
-    d <- read_inputs(glue("scratch/01-trec-download/web{year}/"), breaker = breaker, topk = 1000)
-    meta <- rio::import(glue("data/web{year}.csv"))
-    stopifnot(all(unique(meta$sys) %in% unique(d$sys)) && all(unique(d$sys) %in% unique(meta$sys)))#TODO
-    topics <- sort(unique(d$topic))
+  # Read runs
+  d <- read_inputs(glue("scratch/01-trec-download/web{year}/"), breaker = "random", topk = 1000)
+  meta <- rio::import(glue("data/web{year}.csv"))
+  stopifnot(all(unique(meta$sys) %in% unique(d$sys)) && all(unique(d$sys) %in% unique(meta$sys)))#TODO
+  topics <- sort(unique(d$topic))
 
-    # For every pair of runs within the same group, compute RBO scores
+  # For every pair of runs within the same group, compute RBO scores
 
-    # First, precompute a data frame with all the comparisons' metadata
-    # (will be easier to paralellize)
-    r <- lapply(unique(meta$group), function(g) {
-      group_syss <- meta |> filter(group == g) |> pull(sys) # runs from the same group
-      if(length(group_syss) > 1) {
-        lapply(utils::combn(group_syss, 2, simplify = FALSE), function(syss) {
-          data.frame(sys1 = syss[1], sys2 = syss[2], topic = topics)
-        }) |> bind_rows()
-      }
-    }) |> bind_rows()
+  # First, precompute a data frame with all the comparisons' metadata
+  # (will be easier to paralellize)
+  r <- lapply(unique(meta$group), function(g) {
+    group_syss <- meta |> filter(group == g) |> pull(sys) # runs from the same group
+    if(length(group_syss) > 1) {
+      lapply(utils::combn(group_syss, 2, simplify = FALSE), function(syss) {
+        data.frame(sys1 = syss[1], sys2 = syss[2], topic = topics)
+      }) |> bind_rows()
+    }
+  }) |> bind_rows()
 
-    # And then proceed with actual calculations
-    r <- future_lapply(1:nrow(r), function(i) {
-      sys1 <- r$sys1[i]
-      sys2 <- r$sys2[i]
-      top <- r$topic[i]
+  # And then proceed with actual calculations
+  r <- future_lapply(1:nrow(r), function(i) {
+    sys1 <- r$sys1[i]
+    sys2 <- r$sys2[i]
+    top <- r$topic[i]
 
-      run1 <- d |> filter(sys == sys1, topic == top)
-      run2 <- d |> filter(sys == sys2, topic == top)
+    run1 <- d |> filter(sys == sys1, topic == top)
+    run2 <- d |> filter(sys == sys2, topic == top)
 
-      # Only calculate RBOs when having valid data (no missing or non-numeric scores)
-      if(all(!is.na(run1$score)) && all(!is.na(run2$score)) &&
-        all(!is.nan(run1$score)) && all(!is.nan(run2$score))) {
+    # sorted by docID
+    run1docid <- run1 |> arrange(desc(score), doc)
+    run2docid <- run2 |> arrange(desc(score), doc)
 
-        lapply(.P, function(p) {
-          run1ties <- extract_ranking(run1$doc, run1$score)
-          run2ties <- extract_ranking(run2$doc, run2$score)
+    # Only calculate RBOs when having valid data (no missing or non-numeric scores)
+    if(all(!is.na(run1$score)) && all(!is.na(run2$score)) &&
+       all(!is.nan(run1$score)) && all(!is.nan(run2$score))) {
 
-          data.frame(sys1, sys2, topic = top, p = p,
-                     rbo = rbo(run1$doc, run2$doc, p, "w", "ext"),
-                     rbow = rbo(run1ties, run2ties, p, "w", "ext"),
-                     rboa = rbo(run1ties, run2ties, p, "a", "ext"),
-                     rbob = rbo(run1ties, run2ties, p, "b", "ext"))
-        })
-      }else{
-        data.frame(sys1, sys2, topic = top, p = NA,
-                   rbo = NA,
-                   rbow = NA,
-                   rboa = NA,
-                   rbob = NA)
-      }
-    }) |> bind_rows()
+      lapply(.P, function(p) {
+        run1ties <- extract_ranking(run1$doc, run1$score)
+        run2ties <- extract_ranking(run2$doc, run2$score)
 
-    # Save all
-    path <- glue("output/rbo-trec-{breaker}/")
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
-    rio::export(r, glue("{path}/web{year}.csv"))
-  }
+        data.frame(sys1, sys2, topic = top, p = p,
+                   rbo_rand = rbo(run1$doc, run2$doc, p, "w", "ext"),
+                   rbo_docid = rbo(run1docid$doc, run2docid$doc, p, "w", "ext"),
+                   rbow = rbo(run1ties, run2ties, p, "w", "ext"),
+                   rboa = rbo(run1ties, run2ties, p, "a", "ext"),
+                   rbob = rbo(run1ties, run2ties, p, "b", "ext"))
+      })
+    }else{
+      data.frame(sys1, sys2, topic = top, p = NA,
+                 rbo_rand = NA,
+                 rbo_docid = NA,
+                 rbow = NA,
+                 rboa = NA,
+                 rbob = NA)
+    }
+  }) |> bind_rows()
+
+  # Save all
+  path <- glue("output/rbo-trec/")
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  rio::export(r, glue("{path}/web{year}.csv"))
 }
+
